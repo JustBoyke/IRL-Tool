39d79eff2b2a0d579582dac923d9601a01a30673
JustBoyke <mail@boykevanvugt.nl>
2022-09-26 19:39:05 +0200

    Obs Websocket V5

--- nginx/windows firewall/close.firewall.ports_run.as.admin.bat
+++
@@ -1,6 +0,0 @@
-netsh advfirewall firewall delete rule name="TCP Port 4444 in" protocol=TCP localport=4444
-netsh advfirewall firewall delete rule name="TCP Port 4444 out" protocol=TCP localport=4444
-netsh advfirewall firewall delete rule name="TCP Port 1935 in" protocol=TCP localport=1935
-netsh advfirewall firewall delete rule name="TCP Port 1935 out" protocol=TCP localport=1935
-pause
-exit
